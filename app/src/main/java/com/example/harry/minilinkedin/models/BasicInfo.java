de7bcf0bf2c4c08f0f4b43d3679f5a52124f1d75
mupxq <xuexhy@gmail.com>
2016-11-07 20:27:38 -0800

    final

---
+++ app/src/main/java/com/example/harry/minilinkedin/models/BasicInfo.java
@@ -0,0 +1,12 @@
+package com.example.harry.minilinkedin.models;
+
+/**
+ * Created by Wentao Liu on 2016/9/19.
+ */
+public class BasicInfo {
+
+    public String name;
+
+    public String email;
+
+}
---
+++ app/src/main/java/com/example/harry/minilinkedin/models/Education.java
@@ -0,0 +1,22 @@
+package com.example.harry.minilinkedin.models;
+
+import java.security.PublicKey;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Wentao Liu on 2016/9/19.
+ */
+public class Education {
+
+    public String name;
+
+    public String major;
+
+    public Date startData;
+
+    public Date endtData;
+
+    public List<String> detailed;
+
+}
